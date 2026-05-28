49dd389f9ace159521f8a215e159fff696867365
smpn3sataptawangharjo-stack <smpn3sataptawangharjo@gmail.com>
2026-05-28 21:16:20 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,17 +1,170 @@
 const dataKelulusan = {
-    "1234567890": {
-        "nama": "BUDI SANTOSO",
-        "nisn": "1234567890",
-        "ttl": "Grobogan, 12 Mei 2011",
+    "0117274836": {
+        "nama": "ALDO ARIF EFENDI",
+        "nisn": "0117274836",
+        "ttl": "Grobogan, 5 Februari 2011",
         "status": "LULUS",
-        "pesan": "Selamat! Pertahankan prestasimu di jenjang berikutnya."
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
     },
-    "0987654321": {
-        "nama": "SITI AMINAH",
-        "nisn": "0987654321",
-        "ttl": "Grobogan, 23 Agustus 2010",
+    "0111563011": {
+        "nama": "ALFIAN FAIS AL FARIZI",
+        "nisn": "0111563011",
+        "ttl": "Grobogan, 13 November 2011",
         "status": "LULUS",
-        "pesan": "Selamat! Sukses selalu untuk masa depanmu."
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0113522150": {
+        "nama": "AMANDA AULIA PUTRI",
+        "nisn": "0113522150",
+        "ttl": "Grobogan, 19 Agustus 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0109993284": {
+        "nama": "ARJUNA AKBAR MAULANA",
+        "nisn": "0109993284",
+        "ttl": "Grobogan, 12 November 2010",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0097431510": {
+        "nama": "BAGAS SAPUTRA",
+        "nisn": "0097431510",
+        "ttl": "Grobogan, 14 Desember 2009",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0116041003": {
+        "nama": "BERKAH AJI PRASETIYO",
+        "nisn": "0116041003",
+        "ttl": "Grobogan, 30 Mei 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0102140515": {
+        "nama": "BIMA SAPUTRA",
+        "nisn": "0102140515",
+        "ttl": "Grobogan, 29 April 2010",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0118006823": {
+        "nama": "BUDI IRWANSYAH",
+        "nisn": "0118006823",
+        "ttl": "Grobogan, 30 Juli 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0106694094": {
+        "nama": "DONA AGNESIA",
+        "nisn": "0106694094",
+        "ttl": "Grobogan, 13 September 2010",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0117248303": {
+        "nama": "FAHMI FIRMANSYAH",
+        "nisn": "0117248303",
+        "ttl": "Grobogan, 4 Juni 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0128531253": {
+        "nama": "GILANG PERMANA PUTRA",
+        "nisn": "0128531253",
+        "ttl": "Grobogan, 6 Januari 2012",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0114232593": {
+        "nama": "HANIF MAULANA",
+        "nisn": "0114232593",
+        "ttl": "Grobogan, 12 Desember 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0112234589": {
+        "nama": "IKHSAN NAWAWI",
+        "nisn": "0112234589",
+        "ttl": "Grobogan, 30 April 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0113043487": {
+        "nama": "JEZTA YUTAVINO",
+        "nisn": "0113043487",
+        "ttl": "Grobogan, 18 Agustus 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0114981513": {
+        "nama": "LAILATUS SALAMAH",
+        "nisn": "0114981513",
+        "ttl": "Grobogan, 22 Mei 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0117762846": {
+        "nama": "MELANI NURUL INSANI",
+        "nisn": "0117762846",
+        "ttl": "Grobogan, 25 April 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0118821042": {
+        "nama": "MEYZA IFANA SUSANTI",
+        "nisn": "0118821042",
+        "ttl": "Grobogan, 22 Mei 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0114398283": {
+        "nama": "MOCH RULLY DHIKA PRASTYO",
+        "nisn": "0114398283",
+        "ttl": "Grobogan, 26 Maret 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0112063232": {
+        "nama": "MUHAMMAD MARSELLINO REFANDI",
+        "nisn": "0112063232",
+        "ttl": "Grobogan, 21 Maret 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0117484844": {
+        "nama": "MUHAMMAD MAULANA TAFFA",
+        "nisn": "0117484844",
+        "ttl": "Grobogan, 12 Juli 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0101974085": {
+        "nama": "OKY DWI KUSUMA WIJAYANTI",
+        "nisn": "0101974085",
+        "ttl": "Grobogan, 3 Oktober 2010",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0116636596": {
+        "nama": "RAHMAT MAULANA IBRAHIM",
+        "nisn": "0116636596",
+        "ttl": "Grobogan, 2 April 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0119914799": {
+        "nama": "WISNU PUTRA ARDIANSYAH",
+        "nisn": "0119914799",
+        "ttl": "Grobogan, 8 November 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
+    },
+    "0117368201": {
+        "nama": "YESICA OKTAVIANA PUTRI",
+        "nisn": "0117368201",
+        "ttl": "Grobogan, 26 Oktober 2011",
+        "status": "LULUS",
+        "pesan": "Selamat atas kelulusanmu! Semoga sukses meraih impian dan cita-cita di jenjang pendidikan yang lebih tinggi."
     }
-    // Tambahkan data siswa SMPN 3 Satu Atap Tawangharjo lainnya di sini
 };
